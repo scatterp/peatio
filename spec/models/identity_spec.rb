2d77eecafd4f493a9fdf652a02851a6397fd5086
Ben Sharpe <bsharpe@gmail.com>
2018-01-11 06:09:47 -0700

    get identity spec to pass

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -21,15 +21,16 @@
 require 'spec_helper'
 
 describe Identity do
-  it { should allow_value("pas1Word").for(:password) }
-  it { should allow_value("pas1Wo@d").for(:password) }
-  it { should allow_value("pas1Wo_d").for(:password) }
-  it { should allow_value("123456").for(:password) }
-  it { should_not allow_value("pwd").for(:password) }
+  it {  is_expected.to allow_value("pas1Word").for(:password) }
+  it {  is_expected.to allow_value("pas1Wo@d").for(:password) }
+  it {  is_expected.to allow_value("pas1Wo_d").for(:password) }
+  it {  is_expected.to allow_value("123456").for(:password) }
+  it {  is_expected.not_to allow_value("pwd").for(:password) }
 
   it "should unify email" do
     create(:identity, email: 'foo@example.com')
-    build(:identity, email: 'Foo@example.com').should_not be_valid
+    id = build(:identity, email: 'Foo@example.com')
+    expect(id.valid?).to eq(false)
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,4 +111,8 @@ RSpec.configure do |config|
   config.define_derived_metadata(file_path: Regexp.new("/spec/services/")) do |metadata|
     metadata[:type] = :service
   end
+
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
 end
