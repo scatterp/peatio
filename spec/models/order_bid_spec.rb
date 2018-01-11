84538bd0076b7253f07353f6dbd73300fb3fce93
Ben Sharpe <bsharpe@gmail.com>
2018-01-11 07:20:12 -0700

    get order_bid_spec to pass

--- spec/models/order_bid_spec.rb
+++ spec/models/order_bid_spec.rb
@@ -54,20 +54,20 @@ describe OrderBid do
     end
 
     it "should require a little" do
-      OrderBid.new(volume: '5'.to_d, ord_type: 'market').compute_locked.should == '500'.to_d * OrderBid::LOCKING_BUFFER_FACTOR
+      expect( OrderBid.new(volume: '5'.to_d, ord_type: 'market').compute_locked).to be_d('500'.to_d * OrderBid::LOCKING_BUFFER_FACTOR)
     end
 
     it "should require more" do
-      OrderBid.new(volume: '25'.to_d, ord_type: 'market').compute_locked.should == '2520'.to_d * OrderBid::LOCKING_BUFFER_FACTOR
+      expect( OrderBid.new(volume: '25'.to_d, ord_type: 'market').compute_locked).to be_d('2520'.to_d * OrderBid::LOCKING_BUFFER_FACTOR)
     end
 
     it "should raise error if the market is not deep enough" do
-      expect { OrderBid.new(volume: '50'.to_d, ord_type: 'market').compute_locked }.to raise_error
+      expect { OrderBid.new(volume: '50'.to_d, ord_type: 'market').compute_locked }.to raise_error(RuntimeError)
     end
 
     it "should raise error if volume is too large" do
       expect { OrderBid.new(volume: '30'.to_d, ord_type: 'market').compute_locked }.not_to raise_error
-      expect { OrderBid.new(volume: '31'.to_d, ord_type: 'market').compute_locked }.to raise_error
+      expect { OrderBid.new(volume: '31'.to_d, ord_type: 'market').compute_locked }.to raise_error(RuntimeError)
     end
   end
 
