825bc3b910d4cfddfaf5f86c107e27bf7a88eb2c
scatterp <scatterp321@mailinator.com>
2019-01-31 14:45:40 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -10,24 +10,31 @@ sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install -y bitcoind
 cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
+
+git clone git://github.com/sstephenson/rbenv.git /home/deploy/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git /home/deploy/.rbenv/plugins/ruby-build
+sudo /home/deploy/.rbenv/plugins/ruby-build/install.sh
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-b$
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-rbenv install 2.2.2
-rbenv global 2.2.2
+
+rbenv install 2.5.1
+rbenv global 2.5.1
+
+git -c http.sslVerify=false clone https://galin_princeberk:eQq7Xc4mGPF8@10.10.10.39/galin_princeberk/my-peatio/
+cp -y /home/deploy/my-peatio/* /home/deploy/peatio/current
+
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-gem install bundler
+gem install bundler -v 1.17.0
 rbenv rehash
 read -p "Press [Enter] key to continue..."
 mkdir -p ~/.bitcoin
 cp bitcoin.conf ~/.bitcoin/bitcoin.conf
 dialog --msgbox "enter bitcoind user password settings and save" 10 20
 sudo nano ~/.bitcoin/bitcoin.conf
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |sudo apt-key add -
+
 sudo apt-get update
 sudo apt-get install -y rabbitmq-server
 sudo rabbitmq-plugins enable rabbitmq_management
@@ -48,10 +55,8 @@ sudo apt-get update
 
 # Install Passenger + Nginx
 sudo apt-get install -y --allow-unauthenticated nginx-extras passenger
-#bitcoind
 sudo cp passenger.conf /etc/nginx/passenger.conf
-#sudo echo "include  /etc/nginx/passenger.conf;" >> /etc/nginx/nginx.conf
-#echo "include  /etc/nginx/passenger.conf;" |sudo tee -a /etc/nginx/nginx.conf
+
 cp /etc/nginx/nginx.conf .
 sed -i '64i\passenger_root /usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini;' nginx.conf
 sed -i '65i\passenger_ruby /home/deploy/.rbenv/shims/ruby;' nginx.conf
@@ -60,13 +65,15 @@ echo "remaining steps 10"
 echo "export RAILS_ENV=production" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/peatio
-#git clone https://github.com/ctubio/Krypto-trading-bot.git ~/K
+
 git clone https://github.com/scatterp/peatio.git ~/peatio/current
-#git checkout testing01
+
 cd ~/peatio/current/
 pwd
-#sudo apt-get install -y ruby-bundler
+
 bundle install --without development test --path vendor/bundle
+bundle update
+
 bin/init_config
 dialog --msgbox "enter pusher <US1> settings and save" 10 20
 sudo nano ~/peatio/current/config/application.yml
@@ -77,27 +84,19 @@ sudo nano ~/peatio/current/config/currencies.yml
 cd ~/peatio/current/
 sudo /etc/init.d/mysql stop
 sudo /etc/init.d/mysql start
-bundle exec rake db:setup
-bundle exec rake assets:precompile
-pwd
-#cd /home/deploy/K/
-#pwd
-#make install
-#cd ..
-#git clone https://github.com/scatterp3/peatio.git ~/jpeatio/current
-#cd ~/jpeatio/current
-#rbenv global jruby-9.1.13.0
-#gem install bundler
-#bundle install
+sleep 5;
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/deploy/peatio/current/config/nginx.conf /etc/nginx/conf.d/peatio.conf
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y python-certbot-nginx
 sudo service nginx stop
+bundle exec rake db:setup
+bundle exec rake assets:precompile
 sudo service nginx start
 bundle exec rake daemons:start
-#sudo service nginx status
+
 bundle exec rake daemons:status
 mv pc ..
 echo "you can now setup ssl optionally start bitcoind if you have over 2gb or visit the website (its up and running)"
